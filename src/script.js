9348be5fed0f639be4ffe4674e0f590709493296
HailGanyu <henryyanguk@gmail.com>
2022-08-04 22:30:28 +0100

    removed stats + spacing

--- src/script.js
+++ src/script.js
@@ -32,7 +32,6 @@
         cameraZ = farPlane / 3; 
         fogHex = 0x000000; 
         fogDensity = 0.0007; 
-
         camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
         camera.position.z = cameraZ;
 
@@ -53,7 +52,6 @@
             vertex.x = Math.random() * 2000 - 1000;
             vertex.y = Math.random() * 2000 - 1000;
             vertex.z = Math.random() * 2000 - 1000;
-
             geometry.vertices.push(vertex);
         }
 
@@ -76,7 +74,6 @@
             ]
         ];
         parameterCount = parameters.length;
-
         for (i = 0; i < parameterCount; i++) {
 
             color = parameters[i][0];
@@ -98,9 +95,7 @@
         renderer = new THREE.WebGLRenderer(); 
         renderer.setPixelRatio(window.devicePixelRatio); 
         renderer.setSize(WIDTH, HEIGHT); 
-
         container.appendChild(renderer.domElement); 
-
         window.addEventListener('resize', onWindowResize, false);
         document.addEventListener('mousemove', onDocumentMouseMove, false);
         document.addEventListener('touchstart', onDocumentTouchStart, false);
@@ -119,23 +114,17 @@
 
         camera.position.x += (mouseX - camera.position.x) * 0.05;
         camera.position.y += (-mouseY - camera.position.y) * 0.05;
-
         camera.lookAt(scene.position);
 
         for (i = 0; i < scene.children.length; i++) {
-
             var object = scene.children[i];
-
             if (object instanceof THREE.PointCloud) {
-
                 object.rotation.y = time * (i < 4 ? i + 1 : -(i + 1));
             }
         }
 
         for (i = 0; i < materials.length; i++) {
-
             color = parameters[i][0];
-
             h = (360 * (color[0] + time) % 360) / 360;
             materials[i].color.setHSL(h, color[1], color[2]);
         }
@@ -149,9 +138,7 @@
     }
 
     function onDocumentTouchStart(e) {
-
         if (e.touches.length === 1) {
-
             e.preventDefault();
             mouseX = e.touches[0].pageX - windowHalfX;
             mouseY = e.touches[0].pageY - windowHalfY;
@@ -159,9 +146,7 @@
     }
 
     function onDocumentTouchMove(e) {
-
         if (e.touches.length === 1) {
-
             e.preventDefault();
             mouseX = e.touches[0].pageX - windowHalfX;
             mouseY = e.touches[0].pageY - windowHalfY;
@@ -169,10 +154,8 @@
     }
 
     function onWindowResize() {
-
         windowHalfX = window.innerWidth / 2;
         windowHalfY = window.innerHeight / 2;
-
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
