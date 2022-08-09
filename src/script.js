a8778374dedf71cd60390c181b19a89698131a4a
HailGanyu <henryyanguk@gmail.com>
2022-08-09 16:28:29 +0100

    revert vertex shader

--- src/script.js
+++ src/script.js
@@ -84,17 +84,8 @@
                 uniforms: {
                     size: { value: 10 },
                     scale: { value: 1 },
-                    color: { value: new THREE.Color('maroon') }
+                    color: { value: new THREE.Color('maroon')}
                 },
-                vertexShader: THREE.ShaderLib.points.vertexShader,
-                fragmentShader: `
-    uniform vec3 color;
-    void main() {
-        vec2 xy = gl_PointCoord.xy - vec2(0.5);
-        float ll = length(xy);
-        gl_FragColor = vec4(color, step(ll, 0.5));
-    }
-    `
             });
 
             particles = new THREE.PointCloud(geometry, materials[i]);
