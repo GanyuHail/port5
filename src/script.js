754dbf5b5bc76cff110c20e155d88724bf00bbee
HailGanyu <henryyanguk@gmail.com>
2022-08-18 23:11:16 +0100

    camera Z default

--- src/script.js
+++ src/script.js
@@ -33,7 +33,7 @@
         fogHex = 0x000000;
         fogDensity = 0.0007;
         camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-        //camera.position.z = cameraZ;
+        camera.position.z = cameraZ;
 
         scene = new THREE.Scene();
         scene.fog = new THREE.FogExp2(fogHex, fogDensity);
